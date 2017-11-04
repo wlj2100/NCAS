16e2159ff2f1c36b1c7ce2a2ef33456e2217a614
wlj2100 <wlj2100@gmail.com>
2017-11-04 16:00:58 -0500

    implement client class implement print class add course class

--- src/Management/client.java
+++ src/Management/client.java
@@ -8,17 +8,64 @@ import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-
+import Management.print;
 public class client {
 	//  Database info and credentials
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	static final String DB_URL = "jdbc:mysql://localhost:3306/project3-nudb";
-	static final String USER = "root";
-	static final String PASS = "";
-	
+	protected static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	protected static final String DB_URL = "jdbc:mysql://localhost:3306/project3-nudb";
+	protected static final String USER = "root";
+	protected static final String PASS = "";
+	private static Connection conn = null;
+	private static Statement stmt = null;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		///
+		while (true) {
+			print.print("Enter ID:");
+			Scanner sc1 = new Scanner(System.in);
+			String id = sc1.nextLine();
+			print.print("Enter Password:");
+			Scanner sc2 = new Scanner(System.in);
+			String password = sc2.nextLine();
+			try {
+				Class.forName(JDBC_DRIVER);
+				print.print("connect to database");
+				conn = DriverManager.getConnection(DB_URL, USER, PASS);
+				stmt = conn.createStatement();
+				String sql = "SELECT Password from Student WHERE ID = "  + id;
+				ResultSet rs = stmt.executeQuery(sql);
+				if (rs.next() && password.equals(rs.getString("Password"))) {
+					menu(id);
+				} else {
+					print.print("wrong id or password");
+					continue;
+				}
+			} catch(SQLException se){
+				//Handle errors for JDBC
+			    se.printStackTrace();
+			}catch(Exception e){
+			    //Handle errors for Class.forName
+			    e.printStackTrace();
+			}finally{
+			    //finally block used to close resources
+			    try{
+			    		if(stmt!=null) stmt.close();
+			    }catch(SQLException se2){
+			    }// nothing we can do
+			    try{
+			    		if(conn!=null) conn.close(); 
+			    	} catch(SQLException se){
+			    		se.printStackTrace();
+			    }//end finally try
+			}//end try
+		}
 	}
+	private static void menu(String id) {
+		while (true) {
+			print.print("hello, id:" + id);
+			System.out.println("The courses currenlty enrolled are :\n");
+		}
+	}
+	
 
 }
---
+++ src/Management/course.java
@@ -0,0 +1,11 @@
+package Management;
+
+public class course {
+	private static final String JDBC_DRIVER = client.JDBC_DRIVER;  
+	private static final String DB_URL = client.DB_URL;
+	private static final String USER = client.USER;
+	private static final String PASS = client.PASS;
+	public course(String id) {
+	}
+	
+}
