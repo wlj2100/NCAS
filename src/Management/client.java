3b11e207203f82ef95136c15807afec6837a94ff
wlj2100 <wlj2100@gmail.com>
2017-11-04 15:14:06 -0500

    init

---
+++ src/Management/client.java
@@ -0,0 +1,24 @@
+package Management;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class client {
+	//  Database info and credentials
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost:3306/project3-nudb";
+	static final String USER = "root";
+	static final String PASS = "";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
